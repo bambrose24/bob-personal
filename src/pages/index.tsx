a7bbd748c36cb25400e9ee2ea14c5113de480b77
Bob Ambrose <bambrose24@gmail.com>
2023-02-15 09:22:01 -0500

    starting

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "../utils/api";
+
+const Home: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>Bob Ambrose</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="my-4 flex-col">
+          <h1 className="text-center text-4xl">Bob Ambrose</h1>
+          <div className="">Hi</div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
